0cd8a5418226998a5ae884c3e3d9433e4b3a0526
Nagul75 <nagul697@gmail.com>
2025-05-18 20:56:55 +0530

    Refactor function to controller
    Remove inline function logic of redirecting after successful/failed
    login

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,13 +1,9 @@
 const {Router} = require('express')
-const passport = require('../auth/passport')
 const indexController = require('../controllers/indexController')
 
 const indexRouter = Router()
 
-indexRouter.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-}))
+indexRouter.post("/login", indexController.loginPost)
 indexRouter.post("/signup", indexController.signupPost)
 
 module.exports = indexRouter
