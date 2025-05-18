87f12a2d086ff3ee7646e22d30a5c20d79d38c56
Nagul75 <nagul697@gmail.com>
2025-05-18 19:35:43 +0530

    Validate signup body, implement add user logic

---
+++ controllers/indexController.js
@@ -0,0 +1,55 @@
+const {body, validationResult} = require('express-validator')
+const {PrismaClient} = require('../generated/prisma')
+const bcrypt = require('bcryptjs')
+
+const prisma = new PrismaClient()
+
+const alphError = "must only contain alphabets"
+
+const validateUser = [
+    body("fullname").trim()
+    .isAlpha('en-US', {ignore: ' '}).withMessage("Fullname" + alphError)
+    .isLength({min: 1, max: 255}).withMessage("Fullname must be between 1 and 255 characters"),
+
+    body("username").trim()
+    .isAlpha().withMessage("Username" + alphError)
+    .isLength({min: 1, max: 255}).withMessage("Username must be between 1 and 255 characters"),
+
+    body("password").isLength({min: 1}).withMessage("Password must be minimum 3 characters"),
+
+    body("passwordConfirmation").custom((value, {req}) => {
+        return value === req.body.password
+    }).withMessage("Password confirmation not same as password"),
+
+    body("email").isEmail().withMessage("Invalid email")
+]
+
+const signupPost = [
+    validateUser,
+    async (req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(401).json({success: false, errors: errors.array()})
+        }
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10)
+            const user = await prisma.user.create({
+                data: {
+                    fullname: req.body.fullname,
+                    email: req.body.email,
+                    username: req.body.username,
+                    password: req.body.password
+                }
+            })
+            if(!user){ throw new Error("Error creating user") }
+            res.status(201).json({success: true, message: "User created successfully!"})
+        }
+        catch(err) {
+            return res.status(401).json({success: false, message: err.message || "Some error!"})
+        }
+    }
+]
+
+module.exports = {
+    signupPost
+}
