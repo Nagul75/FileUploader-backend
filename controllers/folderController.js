4fac3aa6391b7e588261e77ef62a93bcb9f2ee78
Nagul75 <nagul697@gmail.com>
2025-05-22 12:47:20 +0530

    Add logic to get children of folders

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -8,14 +8,31 @@ validateFolder = [
      .isLength({min: 1, max: 20}).withMessage("Folder names must be between 3 and 20 characters")
 ]
 
-const readFolderGET = (req, res) => {
-    
+const readFolderGET = async (req, res) => {
+    const user_id = req.params.user_id;
+    const parent_id = req.params.folder_id;
+
+    try {
+        const children = await prisma.folder.findMany({
+            where: {
+                userId: Number(user_id),
+                parent_id: Number(parent_id)
+            },
+        });
+        if(!children) {
+            return res.status(400).json({success: false, message: "Error retrieving folder info"});
+        }
+        return res.status(200).json({success: true, children: children});
+    }
+    catch(err) {
+        return res.status(500).json({success: false, message: err.message || "Internal server error"});
+    }
 }
 
 const createFolderPOST = [
     validateFolder,
     async (req, res) => {
-        const user_id = req.params.id
+        const user_id = req.params.user_id
         const parent_id = req.query.parent_id
         const errors = validationResult(req)
         if(!errors.isEmpty()) {
